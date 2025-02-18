48d582508b10b05e30d0e5a13fd36b09bc025b79
Sobhit Jadoun <62428157+SobhitJadoun@users.noreply.github.com>
2025-02-19 01:25:29 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.owl-carousel.review').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 1,
+    navText: [
+        '<i class="fa-solid fa-arrow-left-long"></i>',
+        '<i class="fa-solid fa-arrow-right-long"></i>'
+    ],
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 800
+});
